90c445a168b08f72e0afdba41dc049cef9fe7dcc
Sergej Charskij <sergej.charskij@gmail.com>
2025-02-08 16:47:31 +0700

    test deployment

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
   return (
     <main>
       <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
+      <button onClick={createTodo}>+ new!</button>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id} onClick={() => deleteTodo(todo.id)}>{todo.content}</li>
